ded7d33b9b57f77e0080c7b945563a68123d8f96
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2021-07-30 10:40:39 -0700

    Update according to cpplint

--- src/radar_processor.cpp
+++ src/radar_processor.cpp
@@ -1,17 +1,22 @@
+/* Copyright (C) 2021 MAciej Rozewicz - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the XYZ license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the XYZ license with
+ * this file. If not, please write to: , or visit :
+ */
+
 #include "radar_processor.hpp"
 
-namespace measurements::radar
-{
-    RadarProcessor::RadarProcessor(void)
-    {
+namespace measurements::radar {
+    RadarProcessor::RadarProcessor(void) {
         int l;
     }
 
-    RadarProcessor::~RadarProcessor(void)
-    {}
+    RadarProcessor::~RadarProcessor(void) {}
 
-    void RadarProcessor::Initialize(const ProcessorCalibration & calibration)
-    {
+    void RadarProcessor::Initialize(const ProcessorCalibration & calibration) {
         calibration_ = calibration;
     }
-}
+}   // namespace measurements::radar
