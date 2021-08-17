7e0bff67bae649297588a7265af6dc4a7ecf8a2d
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2021-08-17 16:34:02 -0700

    Updated velocity estimator tests

--- tests/velocity_estimator_test.cpp
+++ tests/velocity_estimator_test.cpp
@@ -23,6 +23,42 @@ TEST_F(VelocityEstimatorTests, ConstructorTest)
     EXPECT_NO_THROW(velocity_estimator = std::make_unique<measurements::radar::VelocityEstimator>(calibrations_));
 }
 
+TEST_F(VelocityEstimatorTests, RunEmptyScanTest)
+{
+    measurements::radar::RadarScan scan;    
+    measurements::radar::VelocityEstimator velocity_estimator = measurements::radar::VelocityEstimator(calibrations_);
+    auto vp = velocity_estimator.Run(scan);
+
+    EXPECT_FALSE(vp.has_value());
+}
+
+TEST_F(VelocityEstimatorTests, RunOneDetectionest)
+{
+    measurements::radar::RadarScan scan;
+    scan.detections.resize(1u);
+    auto vx = 10.0f;
+    auto vy = 0.5f;
+    std::vector<float> azimuths(scan.detections.size());
+    std::iota(azimuths.begin(), azimuths.end(), -5);
+    
+    std::transform(azimuths.begin(), azimuths.end(),
+        scan.detections.begin(),
+        [=](float azimuth) {
+            measurements::radar::RadarDetection detection;
+            detection.range_rate = vx * std::cos(azimuth) + vy * std::sin(azimuth);
+            detection.range_rate_std = 0.1f;
+            detection.azimuth = azimuth;
+            detection.dealiasing_status = measurements::radar::DealiasingStatus::StaticVelocityProfileDealiased;
+            return detection;
+        }
+    );
+
+    measurements::radar::VelocityEstimator velocity_estimator = measurements::radar::VelocityEstimator(calibrations_);
+    auto vp = velocity_estimator.Run(scan);
+
+    EXPECT_FALSE(vp.has_value());
+}
+
 TEST_F(VelocityEstimatorTests, RunTest)
 {
     measurements::radar::RadarScan scan;
@@ -43,6 +79,37 @@ TEST_F(VelocityEstimatorTests, RunTest)
             return detection;
         }
     );
+    
+    measurements::radar::VelocityEstimator velocity_estimator = measurements::radar::VelocityEstimator(calibrations_);
+    auto vp = velocity_estimator.Run(scan);
+
+    EXPECT_TRUE(vp.has_value());
+    EXPECT_NEAR(vp->vx, vx, 1e-4);
+    EXPECT_NEAR(vp->vy, vy, 1e-4);
+}
+
+TEST_F(VelocityEstimatorTests, RunWithOneOutlierTest)
+{
+    measurements::radar::RadarScan scan;
+    scan.detections.resize(10);
+    auto vx = 10.0f;
+    auto vy = 0.5f;
+    std::vector<float> azimuths(scan.detections.size());
+    std::iota(azimuths.begin(), azimuths.end(), -5);
+    
+    std::transform(azimuths.begin(), azimuths.end(),
+        scan.detections.begin(),
+        [=](float azimuth) {
+            measurements::radar::RadarDetection detection;
+            detection.range_rate = vx * std::cos(azimuth) + vy * std::sin(azimuth);
+            detection.range_rate_std = 0.1f;
+            detection.azimuth = azimuth;
+            detection.dealiasing_status = measurements::radar::DealiasingStatus::StaticVelocityProfileDealiased;
+            return detection;
+        }
+    );
+
+    scan.detections.at(0).range_rate += 10.0f;
     
     measurements::radar::VelocityEstimator velocity_estimator = measurements::radar::VelocityEstimator(calibrations_);
     auto vp = velocity_estimator.Run(scan);
