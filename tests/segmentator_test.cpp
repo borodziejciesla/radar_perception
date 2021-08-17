2d9ce4df2b834d0ca3e78fc5f2ce1d429d34c8b3
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2021-08-17 15:32:43 -0700

    Updated segmentator tests

--- tests/segmentator_test.cpp
+++ tests/segmentator_test.cpp
@@ -67,6 +67,17 @@ TEST_F(SegmentatorTests, ConstructorTest) {
     EXPECT_NO_THROW(auto segmentator = std::make_unique<measurements::radar::Segmentator>(calibration_));
 }
 
+TEST_F(SegmentatorTests, RunEmptyScanTest) {
+    measurements::radar::RadarScan scan;
+    
+    calibration_.neighbourhood_threshold = 100.0f;
+    calibration_.minimum_detection_in_segment = 2u;
+
+    auto segmentator = measurements::radar::Segmentator(calibration_);
+
+    EXPECT_NO_THROW(segmentator.Run(scan));
+}
+
 TEST_F(SegmentatorTests, RunAllDetectionInSingleSegmentTest) {
     measurements::radar::RadarScan scan;
     scan.detections.resize(10);
@@ -149,5 +160,44 @@ TEST_F(SegmentatorTests, RunTwoSegmentsTest) {
             EXPECT_EQ(detection.segment_id, 1u);
         else
             EXPECT_EQ(detection.segment_id, 2u);
+    }
+}
+
+TEST_F(SegmentatorTests, RunSegmentedPointsTest) {
+    measurements::radar::RadarScan scan;
+    scan.detections.resize(10);
+    auto vx = 10.0f;
+    auto vy = 0.5f;
+    std::vector<float> azimuths(scan.detections.size());
+    std::iota(azimuths.begin(), azimuths.end(), -5);
+    
+    std::transform(azimuths.begin(), azimuths.end(),
+        scan.detections.begin(),
+        [=](float azimuth) {
+            static size_t id = 1;
+            measurements::radar::RadarDetection detection;
+            detection.id = id++;
+            detection.range_rate = vx * std::cos(azimuth) + vy * std::sin(azimuth);
+            detection.range_rate_std = 0.1f;
+            detection.azimuth = azimuth;
+            detection.range = 10.0f;
+            detection.x = detection.range * std::cos(detection.azimuth);
+            detection.y = detection.range * std::sin(detection.azimuth);
+            detection.dealiasing_status = measurements::radar::DealiasingStatus::StaticVelocityProfileDealiased;
+            detection.segment_id = 1u;
+            return detection;
+        }
+    );
+
+    calibration_.neighbourhood_threshold = 1000000.0f;
+    calibration_.minimum_detection_in_segment = 2u;
+
+    auto segmentator = measurements::radar::Segmentator(calibration_);
+
+    segmentator.Run(scan);
+
+    for (const auto & detection : scan.detections)
+    {
+        EXPECT_EQ(detection.segment_id, 1u);
     }
 }
