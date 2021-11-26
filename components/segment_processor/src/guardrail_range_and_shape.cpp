a6371a0cd2b4c85027e3a8e558767eefd2e12449
Maciej Rozewicz <rozewicz.maciej@gmail.com>
2021-11-26 01:17:04 +0100

    Added handling guardrail with 1, 2, or 3 detections only - fit lower order polynomial.

--- components/segment_processor/src/guardrail_range_and_shape.cpp
+++ components/segment_processor/src/guardrail_range_and_shape.cpp
@@ -41,34 +41,84 @@ namespace measurements::radar
         range_.end = max_range_;
 
         // Inverse matrix
-        Eigen::MatrixXf a(4, 4);
-        a(0, 0) = a0_;
-        a(0, 1) = a(1, 0) = a1_;
-        a(0, 2) = a(2, 0) = a2_;
-        a(0, 3) = a(3, 0) = a3_;
+        if (ids_.size() >= 4u) {
+            Eigen::MatrixXf a(4, 4);
+            a(0, 0) = a0_;
+            a(0, 1) = a(1, 0) = a1_;
+            a(0, 2) = a(2, 0) = a2_;
+            a(0, 3) = a(3, 0) = a3_;
 
-        a(1, 1) = a2_;
-        a(1, 2) = a(2, 1) = a3_;
-        a(1, 3) = a(3, 1) = a4_;
+            a(1, 1) = a2_;
+            a(1, 2) = a(2, 1) = a3_;
+            a(1, 3) = a(3, 1) = a4_;
 
-        a(2, 2) = a4_;
-        a(2, 3) = a(3, 2) = a5_;
+            a(2, 2) = a4_;
+            a(2, 3) = a(3, 2) = a5_;
 
-        a(3, 3) = a6_;
+            a(3, 3) = a6_;
 
-        Eigen::MatrixXf b(4, 1);
-        b(0) = b1_;
-        b(1) = b2_;
-        b(2) = b3_;
-        b(3) = b4_;
+            Eigen::MatrixXf b(4, 1);
+            b(0) = b1_;
+            b(1) = b2_;
+            b(2) = b3_;
+            b(3) = b4_;
 
-        auto u = a.inverse() * b;
+            auto u = a.inverse() * b;
 
-        guardrail_.range = range_;
-        guardrail_.polynomial.a0 = u(0,0);
-        guardrail_.polynomial.a1 = u(1,0);
-        guardrail_.polynomial.a2 = u(2,0);
-        guardrail_.polynomial.a3 = u(3,0);
+            guardrail_.range = range_;
+            guardrail_.polynomial.a0 = u(0,0);
+            guardrail_.polynomial.a1 = u(1,0);
+            guardrail_.polynomial.a2 = u(2,0);
+            guardrail_.polynomial.a3 = u(3,0);
+        } else if (ids_.size() == 3u) {
+            Eigen::MatrixXf a(3, 3);
+            a(0, 0) = a0_;
+            a(0, 1) = a(1, 0) = a1_;
+            a(0, 2) = a(2, 0) = a2_;
+
+            a(1, 1) = a2_;
+            a(1, 2) = a(2, 1) = a3_;
+
+            a(2, 2) = a4_;
+
+            Eigen::MatrixXf b(3, 1);
+            b(0) = b1_;
+            b(1) = b2_;
+            b(2) = b3_;
+
+            auto u = a.inverse() * b;
+
+            guardrail_.range = range_;
+            guardrail_.polynomial.a0 = u(0,0);
+            guardrail_.polynomial.a1 = u(1,0);
+            guardrail_.polynomial.a2 = u(2,0);
+            guardrail_.polynomial.a3 = 0.0f;
+        } else if (ids_.size() == 2u) {
+            Eigen::MatrixXf a(2, 2);
+            a(0, 0) = a0_;
+            a(0, 1) = a(1, 0) = a1_;
+
+            a(1, 1) = a2_;
+
+            Eigen::MatrixXf b(2, 1);
+            b(0) = b1_;
+            b(1) = b2_;
+
+            auto u = a.inverse() * b;
+
+            guardrail_.range = range_;
+            guardrail_.polynomial.a0 = u(0,0);
+            guardrail_.polynomial.a1 = u(1,0);
+            guardrail_.polynomial.a2 = 0.0f;
+            guardrail_.polynomial.a3 = 0.0f;
+        } else {
+            guardrail_.range = range_;
+            guardrail_.polynomial.a0 = 0.0f;
+            guardrail_.polynomial.a1 = 0.0f;
+            guardrail_.polynomial.a2 = 0.0f;
+            guardrail_.polynomial.a3 = 0.0f;
+        }
+
         guardrail_.assigned_detdectios_ids = ids_;
 
         return guardrail_;
